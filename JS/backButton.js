f719f8cef0a1ed16baf645d897bfb4311f15de36
4min dev <rondo.dolo@bk.ru>
2025-09-28 18:34:10 +0300

    taro

--- JS/backButton.js
+++ JS/backButton.js
@@ -1,44 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const backButton = window.Telegram?.WebApp?.BackButton
+    const backButton = window.Telegram?.WebApp.BackButton
 
     function initBackButton() {
-        if (backButton) {
-            backButton.show()
-            backButton.onClick(() => {
-                history.back()
-            })
-        }
-    }
-
-    function updateBackButton() {
-        if (backButton) {
-            if (window.location.pathname === "/home.html") {
-                backButton.hide()
-            } else {
-                backButton.show()
-                initBackButton()
-            }
-        }
-    }
-
-    updateBackButton()
-
-    document.querySelectorAll('a[href^="/"]').forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault()
-            const href = link.getAttribute('href')
-            history.pushState(null, null, href)
-            updateBackButton()
+        backButton.show()
+        backButton.onClick(() => {
+            history.back()
         })
-    })
+    }
 
-    window.addEventListener('popstate', () => {
-        updateBackButton()
-    })
+    if (window.location.pathname === "/home.html") {
+        backButton.hide()
+    } else {
+        initBackButton()
+    }
 
-    document.querySelector('.hide__back__button')?.addEventListener('click', () => {
-        if (backButton) {
-            backButton.hide()
-        }
-    })
+    document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide())
 })
