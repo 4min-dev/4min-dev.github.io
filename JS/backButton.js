ba178dd40e3e2c64a60444cf934daa2ca5cc3e1d
4min dev <rondo.dolo@bk.ru>
2025-09-29 12:58:10 +0300

    taro

--- JS/backButton.js
+++ JS/backButton.js
@@ -1,18 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const backButton = window.Telegram?.WebApp.BackButton
+    const backButton = window.Telegram?.WebApp.BackButton;
 
     function initBackButton() {
-        backButton.show()
+        backButton.show();
         backButton.onClick(() => {
-            history.back()
-        })
+            history.back();
+        });
     }
 
     if (window.location.pathname === "/home.html") {
-        backButton.hide()
+        backButton.hide();
     } else {
-        initBackButton()
+        initBackButton();
     }
 
-    document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide())
-})
+    document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide());
+
+    // Перехватываем pushState и replaceState
+    const originalPushState = history.pushState;
+    const originalReplaceState = history.replaceState;
+
+    history.pushState = function (...args) {
+        originalPushState.apply(this, args);
+        alert('Страница изменилась!');
+        // Обновляем состояние кнопки
+        if (window.location.pathname === "/home.html") {
+            backButton.hide();
+        } else {
+            initBackButton();
+        }
+    };
+
+    history.replaceState = function (...args) {
+        originalReplaceState.apply(this, args);
+        alert('Страница изменилась!');
+        // Обновляем состояние кнопки
+        if (window.location.pathname === "/home.html") {
+            backButton.hide();
+        } else {
+            initBackButton();
+        }
+    };
+});
