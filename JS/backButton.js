048511627c2e4a51ac19dcd650dd5be7d869f323
4min dev <rondo.dolo@bk.ru>
2025-09-29 13:08:55 +0300

    taro

--- JS/backButton.js
+++ JS/backButton.js
@@ -1,10 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
     const backButton = window.Telegram?.WebApp.BackButton
-    alert('tesss')
+    alert('tessbs')
     function initBackButton() {
         backButton.show()
         backButton.onClick(() => {
             history.back()
+            alert(window.location.pathname)
         })
     }
 
@@ -13,9 +14,9 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
         initBackButton()
     }
-
-    document.addEventListener('pageshow', () => {
-        alert('show')
-    })
     document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide())
+})
+
+document.addEventListener('pageshow', () => {
+    alert('show')
 })
