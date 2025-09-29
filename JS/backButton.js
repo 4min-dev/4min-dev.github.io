78ebb7b32f7ac6acefcba744a96ded56aebb99a2
4min dev <rondo.dolo@bk.ru>
2025-09-29 13:16:42 +0300

    taro

--- JS/backButton.js
+++ JS/backButton.js
@@ -1,30 +1,31 @@
 document.addEventListener('DOMContentLoaded', () => {
-    initializePage();
-});
+    initializePage()
+})
 
 window.addEventListener('pageshow', (event) => {
+    alert('show')
     if (event.persisted) {
-        // Страница восстановлена из кэша (BF Cache)
-        initializePage();
+        alert('cached')
+        initializePage()
     }
-});
+})
 
 function initializePage() {
-    alert('test');
-    const backButton = window.Telegram?.WebApp.BackButton;
+    alert('test')
+    const backButton = window.Telegram?.WebApp.BackButton
 
     function initBackButton() {
-        backButton.show();
+        backButton.show()
         backButton.onClick(() => {
-            history.back();
-        });
+            history.back()
+        })
     }
 
     if (window.location.pathname === "/home.html") {
-        backButton.hide();
+        backButton.hide()
     } else {
-        initBackButton();
+        initBackButton()
     }
 
-    document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide());
+    document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide())
 }
