9da4c8fe138ba15edd3cceaa03621526547647e4
4min dev <rondo.dolo@bk.ru>
2025-09-30 14:59:30 +0300

    taro

--- JS/backButton.js
+++ JS/backButton.js
@@ -1,35 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
-    initializePage()
-})
-
-function initializePage() {
-    const backButton = window.Telegram.WebApp.BackButton
+    const backButton = window.Telegram?.WebApp.BackButton
+    alert(window.location.pathname)
 
     function initBackButton() {
         backButton.show()
-
-        if (window.location.pathname === '/userAgreement.html') {
-            backButton.onClick(() => {
-                window.history.back()
-            })
-        } else {
-            backButton.onClick(() => {
-
-                const previousPage = document.referrer || window.history.state?.url || '/'
-                const urlWithNoCache = previousPage.includes('?')
-                    ? `${previousPage}&nocache=${Date.now()}`
-                    : `${previousPage}?nocache=${Date.now()}`
-                window.location.replace(urlWithNoCache)
-            })
-        }
+        backButton.onClick(() => {
+            history.back()
+        })
     }
 
     if (window.location.pathname === "/home.html") {
         backButton.hide()
     } else {
-        backButton.hide()
         initBackButton()
     }
 
     document.querySelector('.hide__back__button').addEventListener('click', () => backButton.hide())
-}
+})
--- JS/themeRender.js
+++ JS/themeRender.js
@@ -2,7 +2,6 @@ document.addEventListener('DOMContentLoaded', () => {
     const webApp = window.Telegram.WebApp
     const themeParams = webApp.themeParams
     console.log(webApp)
-
     if (webApp.colorScheme === 'dark') {
         document.documentElement.style.setProperty('--agreement__card__title__color', '#101828')
         document.documentElement.style.setProperty('--agreement__card__description__color', '#364153')
@@ -19,10 +18,4 @@ document.addEventListener('DOMContentLoaded', () => {
     document.documentElement.style.setProperty('--primary__hint', themeParams.hint_color)
     document.documentElement.style.setProperty('--primary__input__background', themeParams.bg_color)
     document.documentElement.style.setProperty('--primary__theme__border__color', themeParams.secondary_bg_color)
-
-    const userAgent = navigator.userAgent || navigator.vendor
-
-    if (/iPad|iPhone|iPod/.test(userAgent)) {
-        document.querySelectorAll('.theme__button').forEach((button) => button.classList.add('ios'))
-    }
 })
